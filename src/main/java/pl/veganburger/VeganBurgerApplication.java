55838b0dfcb3b840f43beb6a9065cfedc8b76671
losu5 <losu507@tlen.pl>
2020-11-20 11:18:43 +0100

    Init commit: home.html HomeController class and HomeControllerTest class was made.

---
+++ src/main/java/pl/veganburger/VeganBurgerApplication.java
@@ -0,0 +1,13 @@
+package pl.veganburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganBurgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganBurgerApplication.class, args);
+	}
+
+}
