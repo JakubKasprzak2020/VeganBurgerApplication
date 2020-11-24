7711d128491f47a05f118a2c352ecf9d96c6212d
losu5 <losu507@tlen.pl>
2020-11-24 14:24:24 +0100

    processOrder method for post functions was created in OrderController class

--- src/main/java/pl/veganburger/Controller/OrderController.java
+++ src/main/java/pl/veganburger/Controller/OrderController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.veganburger.Model.Order;
 
@@ -17,6 +18,12 @@ public class OrderController {
     public String orderForm(Model model) {
      model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order has been placed " + order );
+        return "redirect:/";
     }
 
 
