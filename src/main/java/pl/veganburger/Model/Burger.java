d417132049fd03dea637f05ae67db21117a082c9
losu5 <losu507@tlen.pl>
2020-11-24 15:17:04 +0100

    Validation annotations were added to Order and Burger classes.

--- src/main/java/pl/veganburger/Model/Burger.java
+++ src/main/java/pl/veganburger/Model/Burger.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -11,6 +13,10 @@ import java.util.List;
 @NoArgsConstructor
 public class Burger {
 
+    @NotNull
+    @Size(min=4, message = "Name of your Burger should have at least 4 characters.")
     private String name;
+
+    @Size(min=1, message = "Your burger needs at least one ingredient.")
     private List<String> ingredients;
 }
--- src/main/java/pl/veganburger/Model/Order.java
+++ src/main/java/pl/veganburger/Model/Order.java
@@ -1,15 +1,29 @@
 package pl.veganburger.Model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class Order {
+
+    @NotBlank(message = "the \"name and surname\" field is mandatory")
     private String name;
+    @NotBlank(message = "the \"street\" field is mandatory")
     private String street;
+    @NotBlank(message = "the \"city\" field is mandatory")
     private String city;
+    @NotBlank(message = "the \"state\" field is mandatory")
     private String state;
+    @NotBlank(message = "the \"zip code\" field is mandatory")
     private String zip;
+    @CreditCardNumber(message = "this is not proper credit card number")
     private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([0-9][0-9])$", message = "proper format is MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "this is not proper CVV code")
     private String ccCVV;
 }
